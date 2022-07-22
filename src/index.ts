ff546697ab1a8655c9a1724a55fb225307c0f938
Steven <ssteven075@gmail.com>
2022-07-22 22:02:32 +0700

    handle create and join room

--- src/index.ts
+++ src/index.ts
@@ -20,14 +20,42 @@ const io = new Server(server, {
 	},
 });
 
+// rooms will consist of key value pair, key being room id, pair being users inside that room and their corresponding data
+
 io.on("connection", (socket) => {
-	console.log("connected");
+	const sockets = Array.from(io.sockets.sockets).map((socket) => socket[0]);
+	// console.log(io.sockets.adapter.rooms);
+	// console.log(sockets);
+	// console.log(socket.rooms);
+
 	socket.on("disconnect", () => {
-		console.log("user disconnected");
+		// disconnected client id
+		// console.log(socket.id);
+		// const sockets = Array.from(io.sockets.sockets).map((socket) => socket[0]);
+		// console.log(io.sockets.adapter.rooms.get("8a7e5d"));
 	});
 
-	socket.on("hi", (data) => {
-		socket.emit("hello", data);
+	socket.on("join room", ({ roomId, username }: { roomId: string; username: string }) => {
+		const room = io.sockets.adapter.rooms.get(roomId);
+		if (!room) {
+			socket.emit("set room owner");
+		}
+		socket.join(roomId);
+		socket.to(roomId).emit("notify", `${username} just joined the room.`);
+	});
+
+	socket.on("create room", (roomId: string) => {
+		if (io.sockets.adapter.rooms.get(roomId)) {
+			socket.emit("room already exist");
+		} else {
+			socket.join(roomId);
+			socket.join(roomId);
+			socket.emit("create room success", roomId);
+			// console.log(roomId);
+			// console.log(io.sockets.adapter.rooms.get(roomId));
+			// const sockets = Array.from(io.sockets.sockets).map((socket) => socket[0]);
+			// console.log("room created: ", socket.rooms);
+		}
 	});
 });
 
